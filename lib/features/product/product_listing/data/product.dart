8527686c88eba693444312f27541e5a9c7292c33
guptaShantanu <officialshantanugupta08@gmail.com>
2023-10-18 19:45:22 +0530

    ADD : product data model

---
+++ lib/features/product/product_listing/data/product.dart
@@ -0,0 +1,31 @@
+class Product {
+  final int id;
+  final String image;
+  final String name;
+  final DateTime? createdAt;
+
+  Product(
+      {required this.id,
+      required this.image,
+      required this.name,
+      this.createdAt});
+
+  factory Product.fromJson(Map json) {
+    return Product(
+        id: json['id'],
+        image: json['image'],
+        name: json['name'],
+        createdAt: json['createdAt'] != null
+            ? DateTime.tryParse(json['createdAt'])
+            : null);
+  }
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> json = {};
+    json['id'] = id;
+    json['image'] = image;
+    json['name'] = name;
+    json['createdAt'] = createdAt?.toIso8601String();
+    return json;
+  }
+}
